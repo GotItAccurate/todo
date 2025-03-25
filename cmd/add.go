815a09f26e6b9e7947d28dc0bb73585922725dcd
aadhii-yz <aadhii.yz@gmail.com>
2025-03-25 17:36:46 +0530

    Added logic to the add command.

--- cmd/add.go
+++ cmd/add.go
@@ -5,10 +5,39 @@ Copyright © 2025 NAME <aadhii.yz@gmail.com>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
+
+const TodoFile = "todos.json"
+
+type Todo struct {
+	Task      string    `json:"task"`
+	CreatedAt time.Time `json:"createdAt"`
+	Deadline  time.Time `json:"deadline"`
+	Done      bool      `json:"done"`
+}
+
+func loadTodos() []Todo {
+	file, err := os.ReadFile(TodoFile)
+	if err != nil {
+		return []Todo{}
+	}
+
+	var todos []Todo
+	json.Unmarshal(file, &todos)
+
+	return todos
+}
+
+func saveTodos(todos []Todo) {
+	data, _ := json.MarshalIndent(todos, "", " ")
+	os.WriteFile(TodoFile, data, 0644)
+}
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
@@ -16,7 +45,40 @@ var addCmd = &cobra.Command{
 	Short: "Add a TODO.",
 	Long:  `Add a TODO to the existing list.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		if len(args) == 0 {
+			fmt.Printf("Provide a Task.\n")
+			return
+		}
+
+		task := args[0]
+
+		var deadline time.Time
+		if len(args) > 1 {
+			parsedDeadline, err := time.Parse("2006-01-02", args[1])
+			if err != nil {
+				fmt.Printf("Invalid deadline format. Use YYYY-MM-DD.\n")
+				return
+			}
+
+			deadline = parsedDeadline
+		} else {
+			deadline = time.Now().Add(24 * time.Hour)
+		}
+
+		todos := loadTodos()
+
+		todos = append(
+			todos,
+			Todo{
+				Task:      task,
+				CreatedAt: time.Now(),
+				Deadline:  deadline,
+				Done:      false,
+			})
+
+		saveTodos(todos)
+
+		fmt.Printf("Added: %s\n", task)
 	},
 }
 
